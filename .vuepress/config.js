0d1e0c73a112e7be79399b8b3ec51c5bfc3f109e
Scott Bellware <sbellware@gmail.com>
2018-11-01 14:01:27 -0500

    WOrk in progress notification is removed

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -23,7 +23,6 @@ module.exports = {
       },
       {
         text: 'User Guide', items: [
-          { text: 'Work in Progress', link: '/user-guide/' },
           { text: 'Message Store', link: '/user-guide/message-store/' },
           { text: 'Handlers', link: '/user-guide/handlers.md' },
           { text: 'Messages and Message Data', link: '/user-guide/messages-and-message-data/' },
@@ -163,6 +162,16 @@ module.exports = {
             'entity-cache.md',
             'snapshotting.md',
             'substitute.md'
+          ]
+        }
+      ],
+      '/user-guide/logging/': [
+        {
+          title: 'Logging',
+          collapsable: false,
+          children: [
+            '',
+            'log-tags.md'
           ]
         }
       ]
