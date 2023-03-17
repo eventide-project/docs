1020208d733b372878abc34c549a45a2ac680e39
Nathan Ladd <nathanladd@gmail.com>
2023-03-17 16:09:21 -0500

    Remove setting of NODE_OPTIONS

--- serve.sh
+++ serve.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-# Fall back to legacy OpenSSL, since OpenSSL v3 is incompatible with webpack - Nathan, Fri Mar 17 2023
-export NODE_OPTIONS=--openssl-legacy-provider
-
 vuepress dev
