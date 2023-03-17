47ce30f00d1475bf0e142a0a2fe4aa7d6f708d99
Nathan Ladd <nathanladd@gmail.com>
2023-03-17 16:06:03 -0500

    OpenSSL v3 is supported

--- serve.sh
+++ serve.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
+# Fall back to legacy OpenSSL, since OpenSSL v3 is incompatible with webpack - Nathan, Fri Mar 17 2023
+export NODE_OPTIONS=--openssl-legacy-provider
+
 vuepress dev
