4f927032b90b369a1267a8a8558b953aff50ab3e
Nathan Ladd <nathanladd@gmail.com>
2025-07-07 17:58:37 -0400

    Upload script is updated to invalidate CloudFront cache

--- upload.sh
+++ upload.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
 
-aws s3 sync _build s3://docs.eventide-project.org --profile brightworks
+domain_name="docs.eventide-project.org"
+echo "Target Domain: $domain_name"
+
+distribution_id=$(aws cloudfront list-distributions \
+  --query "DistributionList.Items[?Origins.Items[?contains(DomainName, '$domain_name.s3.')]].Id" \
+  --output text)
+
+echo "Distribution ID: $distribution_id"
+
+cmd="aws s3 sync _build s3://$domain_name --profile brightworks"
+echo "$cmd"
+eval "$cmd"
+
+cmd="aws cloudfront create-invalidation --distribution-id $distribution_id --paths '/*'"
+echo "$cmd"
+eval "$cmd"
